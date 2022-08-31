48de8cd25fe0d9cb0bfe6ad30dadd4c9d22f9794
MaweniLive <saliwalive@gmailcom>
2022-08-31 10:23:05 +0200

    complsd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.set("port", process.env.PORT || 2121);
+app.set("port", process.env.PORT || 6969);
 app.use(express.json());
 app.use(cors());
 
