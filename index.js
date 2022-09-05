60cf03d9c9c7ea4a0e5658f1a767a07d3b9be785
MaweniLive <saliwalive@gmailcom>
2022-09-05 11:33:30 +0200

    hgshiad

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ app.use(express.json());
 app.use(cors());
 
 app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.json({msg: "Welcome"});
+});
 
 app.listen(app.get("port"), () => {
   console.log(`Listening for calls on port ${app.get("port")}`);
